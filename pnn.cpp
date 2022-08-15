98dab4ed2afa479f9ee9058476bde2d7db795202
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2022-08-15 12:12:05 -0300

    write a number, the aplication will tell you if its positive, negative or nule

---
+++ pnn.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main ()
+
+{   int num;
+    cout << "Digite o número: ";
+    cin >> num;
+    if (num > 0)
+    {
+        cout << "Esse número é positivo";
+    }
+
+    else if (num < 0)
+    {
+        cout << "Esse número é negativo";
+    }
+
+    else
+{
+    cout << "Esse número é nulo";
+}
+    
+
+}
